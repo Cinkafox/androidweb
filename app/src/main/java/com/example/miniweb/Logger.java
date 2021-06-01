dff654b973c7fb3900aa9df1314d03ccfed950dc
Cinkafox <leofoxm40@gmail.com>
2021-06-01 20:01:12 +0300

    commit

--- app/src/main/java/com/example/miniweb/Logger.java
+++ app/src/main/java/com/example/miniweb/Logger.java
@@ -5,7 +5,7 @@ import android.widget.TextView;
 public class Logger {
     public static String textt ="";
     public static void setText(String text){
-        textt = textt + "Server > " + text + "\n";
+        textt = textt + "Server > " + text + " \n";
     }
     public static String getText(){
         return textt;
